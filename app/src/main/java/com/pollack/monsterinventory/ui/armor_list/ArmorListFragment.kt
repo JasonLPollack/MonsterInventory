43e57e043c17e048efae2e47b5fa314bff98a995
Jason Pollack <jlpollack@gmail.com>
2020-10-02 11:25:34 -0400

    Handle load/error states

--- app/src/main/java/com/pollack/monsterinventory/ui/armor_list/ArmorListFragment.kt
+++ app/src/main/java/com/pollack/monsterinventory/ui/armor_list/ArmorListFragment.kt
@@ -2,15 +2,18 @@ package com.pollack.monsterinventory.ui.armor_list
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.SearchView
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.pollack.monsterinventory.R
 import com.pollack.monsterinventory.domain.ArmorPart
-import com.pollack.monsterinventory.ui.*
+import com.pollack.monsterinventory.ui.ArmorDataError
+import com.pollack.monsterinventory.ui.ArmorDataPopulated
+import com.pollack.monsterinventory.ui.ArmorDataUninitialized
+import com.pollack.monsterinventory.ui.ItemsListModel
 import com.pollack.util.TAG
 import com.pollack.util.hideBackButton
 import kotlinx.android.synthetic.main.fragment_armor_list.*
@@ -30,11 +33,32 @@ class ArmorListFragment : Fragment(R.layout.fragment_armor_list) {
         armor_list.layoutManager = LinearLayoutManager(requireContext())
 
         model.armorDataState.observe(viewLifecycleOwner) { state ->
+            var loadingCardVisibility = View.GONE
+            var errorCardVisibility = View.GONE
+            var listVisibility = View.GONE
+
             when (state) {
-                is ArmorDataPopulated -> onHaveParts(state.items)
-                is ArmorDataError -> onRetrievalError()
-                is ArmorDataUninitialized -> model.loadItems()
+                is ArmorDataPopulated -> {
+                    listVisibility = View.VISIBLE
+                    onHaveParts(state.items)
+                }
+                is ArmorDataError -> {
+                    errorCardVisibility = View.VISIBLE
+                    onRetrievalError()
+                }
+                is ArmorDataUninitialized -> {
+                    loadingCardVisibility = View.VISIBLE
+                    onUninitialized()
+                }
             }
+
+            loading_card.visibility = loadingCardVisibility
+            error_card.visibility = errorCardVisibility
+            main_list_layout.visibility = listVisibility
+        }
+
+        btn_try_again.setOnClickListener {
+            model.reset()
         }
 
         model.filterBy.observe(viewLifecycleOwner) { filterBy ->
@@ -75,5 +99,9 @@ class ArmorListFragment : Fragment(R.layout.fragment_armor_list) {
 
     protected fun onRetrievalError() {
         Log.v(TAG, "Oh noes!")
+    }
+
+    protected fun onUninitialized() {
+        model.loadItems()
     }
 }
